df138b3321de74e23e73005157b0197aaccc52b2
Christian Hedz <cristian.28hedz@gmail.com>
2025-03-16 12:22:27 -0600

    feat: agregar boton de logout en el toolbar

--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -7,7 +8,7 @@ import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
-  imports: [MatToolbarModule,MatIconModule, MatMenuModule,MatMenuTrigger,RouterModule],
+  imports: [MatToolbarModule,MatIconModule, MatMenuModule,MatMenuTrigger,MatButtonModule, RouterModule],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css'
 })
