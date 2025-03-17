cb7d105ef75ff122568397e24d5d6fc9d9ed1eee
Christian Hedz <cristian.28hedz@gmail.com>
2025-03-17 14:05:02 -0600

    style:  eliminar comentarios y quitar codigo css del html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,17 +6,14 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { isAuthGuard } from './auth/guards/is-auth.guard';
 import { isNotAuthGuard } from './auth/guards/is-not-auth.guard';
 import { InfoComponent } from './pages/info/info.component';
-import { PruebaComponent } from './pages/prueba/prueba.component';
 import { isAdminGuard } from './auth/guards/is-admin.guard';
 
 export const routes: Routes = [
   // { path: 'inicio', redirectTo: 'admin/inicio', pathMatch: 'full' },
   { path: '', redirectTo:'login', pathMatch:'full' },
 
-  { path: 'registro', component: RegisterComponent, canActivate: [isNotAuthGuard] },
-  // { path: 'admin/inicio',component: DashboardComponent, canMatch: [isAuthGuard]  },
-  // { path: 'admin/info',component: InfoComponent, canMatch: [isAuthGuard] },
-  // { path: 'admin/prueba',component: PruebaComponent},
+  { path: 'registro', component: RegisterComponent },
+
   { path: 'login',component: LoginComponent, canMatch: [isNotAuthGuard] },
 
   {
@@ -32,11 +29,11 @@ export const routes: Routes = [
         component: InfoComponent,
         canMatch: [isAuthGuard],
       },
-      {
-        path: 'prueba',
-        loadComponent: () => import('./pages/prueba/prueba.component')
-          .then(m => m.PruebaComponent),
-      },
+      // {
+      //   path: 'prueba',
+      //   loadComponent: () => import('./pages/prueba/prueba.component')
+      //     .then(m => m.PruebaComponent),
+      // },
       {
         path: '',
         redirectTo: 'inicio',
