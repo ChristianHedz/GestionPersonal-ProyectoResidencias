18d9b05301d5695c5492d58725c75f986672f5ee
Christian Hedz <cristian.28hedz@gmail.com>
2025-03-19 19:21:51 -0600

    fix: arregle error de scroll y logo muy grande en pantallas menores a 578px

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -45,10 +45,10 @@ export class LoginComponent {
       ]],
       password: ['', [Validators.required]],
     });
-    
-    // Propiedades computadas
+
     email = computed(() => this.loginForm.controls.email);
     password = computed(() => this.loginForm.controls.password);
+    
 
     login(): void {
       if (this.loginForm.invalid) {
@@ -56,8 +56,7 @@ export class LoginComponent {
         Swal.fire('Error!', 'Por favor completa correctamente el formulario', 'error');
         return;
       }
-      
-  
+        
       this.authService.login(this.loginForm.value as LoginResponse)
         .subscribe({
           next: (employee: AuthResponse) => {
@@ -72,8 +71,7 @@ export class LoginComponent {
         });
     }
     
-    // Métodos de validación
-    isValidField(field: string): boolean {
+        isValidField(field: string): boolean {
       const control = this.loginForm.get(field);
       return !!control && control.invalid && control.touched;
     }
@@ -85,8 +83,7 @@ export class LoginComponent {
       
       if (control.errors['required']) return 'Este campo es requerido';
       if (control.errors['pattern']) {
-        if (field === 'email') return 'Ingresa un correo electrónico válido';
-        return 'El formato ingresado no es válido';
+        return field === 'email' ? 'Ingresa un correo electrónico válido' : 'El formato ingresado no es válido';
       }
       
       return 'Campo inválido';
--- src/app/pages/info/info.component.ts
+++ src/app/pages/info/info.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { ToolbarComponent } from "../../shared/toolbar/toolbar.component";
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-info',
-  imports: [ToolbarComponent,CommonModule],
+  imports: [ToolbarComponent,CommonModule,RouterModule,MatToolbarModule],
   templateUrl: './info.component.html',
   styleUrl: './info.component.css',
 })
